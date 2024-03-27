ef443882a06e1744ef6c856c0924d09f1e7252bf
Andrew Brantley <git@andbrant.com>
2024-03-27 10:13:40 -0500

    add env var for vaultwarden server

---
+++ start.sh
@@ -0,0 +1,5 @@
+env_file=".env";
+if [ ! -e "$env_file" ]; then
+    source "$env_file";
+fi
+docker-compose up;
--- update.sh
+++ update.sh
@@ -1,1 +1,5 @@
+env_file=".env";
+if [ ! -e "$env_file" ]; then
+    source "$env_file"
+fi
 docker-compose pull && docker-compose restart &
