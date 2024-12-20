56af732d1b58a04e177fc052e342ef5e40b14e9e
Andrew Brantley <git@andbrant.com>
2024-12-20 10:05:29 -0600

    add check for VAULTWARDEN_PATH

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 env_file=".env";
+
 if [ ! -e "$env_file" ]; then
     source "$env_file";
 fi
+
+if [ -z $VAULTWARDEN_PATH ]; then
+  echo "VAULTWARDEN_PATH is not defined; exiting"
+  exit 
+fi
+
 docker-compose up -d;
